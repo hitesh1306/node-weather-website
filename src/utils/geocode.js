96c618cb70972358ec67e25a29b9fd13f127541a
Hitesh Jambhulkar <hiteshjambhulkar@Hiteshs-MacBook-Air.local>
2019-12-11 20:40:36 +0530

    Init commit

---
+++ src/utils/geocode.js
@@ -0,0 +1,24 @@
+const request = require("request");
+
+const geocode = (address, callback) => {
+  const url =
+    "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+    address +
+    ".json?access_token=pk.eyJ1IjoiaGl0ZXNoajEzMDYiLCJhIjoiY2szcmNrY3UzMGE0eTNqbW4wdDk3a3A5cyJ9.gFncz-C1bvQwk2RRtSvgnA&limit=1";
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to location services!", undefined);
+    } else if (body.features.length === 0) {
+      callback("Unable to find location. Try another search.", undefined);
+    } else {
+      callback(undefined, {
+        latitude: body.features[0].center[1],
+        longitude: body.features[0].center[0],
+        location: body.features[0].place_name
+      });
+    }
+  });
+};
+
+module.exports = geocode;
